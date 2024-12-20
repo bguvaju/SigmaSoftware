95a6d9364552994519ddcd00147aedc705ec17bd
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-20 14:11:11 +0545

    Candidate model added and unwanted controller removed

---
+++ Data/Data/Model/Candidate.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Model
+{
+    public class Candidate : BaseEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        public DateTime? TimeToCall { get; set; }
+
+        public string? LinkedInProfile { get; set; }
+
+        public string? GithubProfile { get; set; }
+
+        public string? Comment { get; set; }
+    }
+}
