86390a7480bf0fcd5c5e2cb4f9ddbf2ce8267328
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-20 14:07:33 +0545

    Base class added

---
+++ Data/Data/Model/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public class BaseEntity
+    {
+        [Key]
+        public string Id { get; set; }
+        public string? Created_By { get; set; }
+        public DateTime? Created_At { get; set; } = DateTime.UtcNow;
+        public string? Updated_By { get; set; }
+        public DateTime? Updated_At { get; set; }
+    }
+}
