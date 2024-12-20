abb2b44ce2f4542ce31be6dd28df6294d269ed9e
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-20 14:32:53 +0545

    DbContext implemented

---
+++ Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
