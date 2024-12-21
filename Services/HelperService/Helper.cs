8f8f56c06805120e1fc734c1c9d1d8ae64b9fd94
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-21 15:35:14 +0545

    Helper Service Added for Logging

---
+++ Services/HelperService/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.HelperService
+{
+    public class Helper
+    {
+        public static async Task<dynamic> ErrorLog(string errMsg)
+        {
+            try
+            {
+                string path = string.Empty;
+                string folder = string.Empty;
+                folder = DateTime.UtcNow.ToString("yyyMMdd");
+                path = Directory.GetCurrentDirectory() + @"/../Error/" + folder + "/";
+                _ = Directory.CreateDirectory(path);
+                await File.WriteAllTextAsync(path + DateTime.UtcNow.ToString("yyyyMMddHHmmssFFF") + "_error.txt", errMsg.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+            return 0;
+        }
+    }
+}
