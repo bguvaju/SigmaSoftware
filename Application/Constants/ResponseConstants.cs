f8e99c34c28ba4e6c0dc066f1cc41f33d28b2ff3
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-21 16:13:31 +0545

    Response file updated

---
+++ Application/Constants/ResponseConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Constants
+{
+    public static class ResponseConstants
+    {
+        public const string ValidationError = "Validation Error";
+        public const string CandidateCreateError = "Error on creating candidate";
+        public const string CandidateCreateSuccess = "Candidate created successfully";
+        public const string CandidateUpdateSuccess = "Candidate updated successfully";
+    }
+}
