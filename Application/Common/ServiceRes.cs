a45ed3193f3bd21e91058163483dd15671c4d3ee
Bikram Guvaju <bguvaju@hotmail.com>
2024-12-21 15:35:36 +0545

    Response Helper added

---
+++ Application/Common/ServiceRes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common;
+
+public class ServiceRes
+{
+    public int code { get; set; }
+    public string message { get; set; }
+    public string description { get; set; }
+}
+
+public class ServiceResData<T> : ServiceRes
+{
+    public T Data { get; set; }
+}
+
+public class ServiceResList<T> : ServiceRes
+{
+    public List<T> Data { get; set; }
+}
